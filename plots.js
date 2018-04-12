ee8d490a34bd14b2b14eafd9195a4c9b8bc07173
artina-m <am2495@cornell.edu>
2018-04-12 14:41:31 -0400

    Animation

--- plots.js
+++ plots.js
@@ -1,21 +1,28 @@
 let plot_orbits = function(className) {
+    // This function plots the spaceSVG and orbit levels
+    
     spaceSVG = d3.select("."+String(className))
       .append("svg")
       .attr("class", "spaceSVG")
-      .attr("width", 600)
+      .attr("id", "space")
+      .attr("width", 700)
       .attr("height", 600)
       .style("background-color", "black")
       .attr("height", "100%")
       .style("background-color", colorTheme);
     
-    let ringcolor = "lightgrey";
+    satelliteGroup = spaceSVG.append("g");
 
+    let ringcolor = "#3F3F3F";
+    let centerPoint = 350;
+    
     let randomCountries = spaceSVG.append("circle")
                 .attr("cx", centerPoint)
                 .attr("cy", centerPoint)
                 .attr("r", 50)
-                .attr("fill", "grey");
-
+                .attr("fill", "black")
+                .attr("stroke", "lightgrey");
+    
     let orbit1 = spaceSVG.append("circle")
                 .attr("class", "LEO")
                 .attr("cx", centerPoint)
@@ -43,60 +50,128 @@ let plot_orbits = function(className) {
 
 
 let plot_satellites = function(d) {
-    let r = 1.2;
-    let color = "white" // initialize category color to be white
+    // Plot satellites as points in respective orbit level
+    // ID = satellite name
+    let r = 1.2;              // Radius of satelllities
+    let centerPoint = 350;    // Animation starting point
+    let color = "white" 
+    let angle = Math.random()  * (2*Math.PI)  // Angle == Country ?
+    let satSpeed = 1000;
     
-    // Color == Satellite Type
-    if (d.users.indexOf("Civil") > -1) { color =  "yellow"}
-    else if (d.users.indexOf("Military") > -1) { color =  "red"}
-    else if (d.users.indexOf("Commercial") > -1) { color =  "blue"}
-    else if (d.users.indexOf("Government") > -1) { color =  "white"}
-
-
-    // Angle == Country 
-    angle = Math.random()  * (2*Math.PI)
-
 
     // Radial position by Orbit type + noise for scatter
     if (d.orbitClass == "LEO"){
         radius = 100 + (Math.random() * 80);
-        var dot = spaceSVG.append("circle")
+        var dot = satelliteGroup.append("circle")
+            .attr("class", "satPoint")
             .attr("id", d.satName)
             .attr("cx", 0 + centerPoint)
             .attr("cy", 0+ centerPoint)
             .attr("r", r)
-            .attr("fill", color);
+            .attr("fill", color)
+            .style("opacity", 0);
         
         dot.transition()
-            .style("transition", 10)
+            .duration(satSpeed)
             .attr("cx", radius * Math.cos(angle) + centerPoint)
             .attr("cy", radius * Math.sin(angle) + centerPoint)
-            
+            .style("opacity", 1);
 
-
-        
     }
 
     else if (d.orbitClass == "MEO"){
-        //radius = 200 + (Math.random() * 20)
-        radius = 0;
-        spaceSVG.append("circle")
+        radius = 200 + (Math.random() * 20)
+        dot = satelliteGroup.append("circle")
+            .attr("class", "satPoint")
             .attr("id", d.satName)
-            .attr("cx", radius * Math.cos(angle) + centerPoint)
-            .attr("cy", radius * Math.sin(angle) + centerPoint)
+            .attr("cx", 0 + centerPoint)
+            .attr("cy", 0 + centerPoint)
             .attr("r", r)
             .attr("fill", color)
+            .style("opacity", 0);
+        
+        dot.transition()
+            .duration(satSpeed)
+            .attr("cx", radius * Math.cos(angle) + centerPoint)
+            .attr("cy", radius * Math.sin(angle) + centerPoint)
+            .style("opacity", 1);
         
     }
+    
      else if (d.orbitClass == "GEO"){
-        //radius = 250 + (Math.random() * 30)
-        radius = 0;
-        spaceSVG.append("circle")
+        radius = 250 + (Math.random() * 30)
+        dot = satelliteGroup.append("circle")
+            .attr("class", "satPoint")
             .attr("id", d.satName)
-            .attr("cx", radius * Math.cos(angle) + centerPoint)
-            .attr("cy", radius * Math.sin(angle) + centerPoint)
+            .attr("cx", 0 + centerPoint)
+            .attr("cy", 0+ centerPoint)
             .attr("r", r)
-            .attr("fill", color)
+            .attr("fill", color).style("opacity", 0);
+        
+        dot.transition()
+            .duration(satSpeed)
+            .attr("cx", radius * Math.cos(angle) + centerPoint)
+            .attr("cy", radius * Math.sin(angle) + centerPoint).style("opacity", 1);
     }
 
 }
+
+
+let filterByType = function(){
+    let angleData = [];
+    let radiusData = [];
+    let allSats = satelliteGroup.selectAll("circle").data(satData)
+    
+    // Update the colors based on use case 
+    // Commericial: Green
+    // Governemnt: Blue
+    // Military: Red
+    // Civil: White
+    allSats.transition().duration(2000)
+    .attr("fill", function(d){
+        let color = "white"
+        if (d.users.indexOf("Civil") > -1) { color =  "white"}
+        else if (d.users.indexOf("Military") > -1) { color =  "#EC7063"}
+        else if (d.users.indexOf("Commercial") > -1) { color =  "#76D7C4"}
+        else if (d.users.indexOf("Government") > -1) { color =  "#0288D1"}
+        return color
+    })
+    .attr("cx", function(d){
+        let centerPoint = 350;
+        angle = 2*Math.PI
+        if (d.users.indexOf("Civil") > -1) { 
+            angle = Math.random()  * (1*Math.PI/8)
+        }
+        else if (d.users.indexOf("Military") > -1) { 
+            angle = Math.random()  * (3.50*Math.PI/8 - 1*Math.PI/8) +  1*Math.PI/8
+        }
+        else if (d.users.indexOf("Commercial") > -1) { 
+            angle = Math.random()  * (13.50*Math.PI/8 - 3.50*Math.PI/8) +  3.5*Math.PI/8
+        }
+        else if (d.users.indexOf("Government") > -1) { 
+            angle = Math.random()  * (16*Math.PI/8 - 13.5*Math.PI/8) +  13.5*Math.PI/8
+        }
+        
+        if (d.orbitClass == "GEO") { radius = 250 + (Math.random() * 30)}
+        else if (d.orbitClass == "MEO") {radius = 200 + (Math.random() * 20)}
+        else if (d.orbitClass == "LEO") { radius = 100 + (Math.random() * 80)}
+        
+        angleData.push(angle); radiusData.push(radius);
+        return radius * Math.cos(angle) + centerPoint
+    })
+    
+    .attr("cy", function(d,i){
+        let centerPoint = 350;
+        radius = radiusData[i];
+        angle = angleData[i];
+        return radius * Math.sin(angle) + centerPoint
+        
+    })
+    
+
+    }
+
+    
+        
+
+
